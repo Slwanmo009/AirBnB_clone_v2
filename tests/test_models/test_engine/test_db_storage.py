5076d7ebf19eb85172954db11cf471b71d7dc59f
slwanmo009 <slwanmohammed9@gmail.com>
2024-06-26 15:49:50 -0700

    hop

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
-"""
-Unittests for models/engine/db_storage.py
-"""
+"""test for file storage"""
 import unittest
-from models.engine.db_storage import DBStorage
+import pep8
 
 
 class TestDBStorage(unittest.TestCase):
-    """
-    Test cases for the DBStorage class
-    """
+    '''Tests the DBStorage storage engine'''
 
-    def test_instance(self):
-        """
-        Test if storage is an instance of DBStorage
-        """
-        storage = DBStorage()
-        self.assertIsInstance(storage, DBStorage)
+    def setUp(self):
+        """SetUp env for test"""
+        pass
 
+    def tearDown(self):
+        """teardown"""
+        pass
+
+    def test_all(self):
+        """Test class method all()"""
+        pass
+
+    def test_new(self):
+        """Test new method"""
+        pass
+
+    def test_save(self):
+        """Test save method
+        """
+        pass
+
+    def test_delete(self):
+        """Tests delete
+        """
+        pass
+
+    def test_reload(self):
+        """Tests reload
+        """
+        pass
 
 if __name__ == "__main__":
     unittest.main()
