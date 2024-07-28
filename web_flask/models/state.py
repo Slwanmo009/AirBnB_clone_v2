77cc60db3b86b72afe6683673509622445411e61
slwanmo009 <slwanmohammed9@gmail.com>
2024-07-28 15:11:09 -0700

    ooo

--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 """
-State module.
+State Module for HBNB project
 """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 import models
 
 
 class State(BaseModel, Base):
-    """
-    State class.
-    """
+    """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+    cities = relationship(
+        'City', backref='state', cascade='all, delete, delete-orphan'
+    )
 
-    @property
-    def cities(self):
-        """
-        Getter method for cities if the storage engine is not DBStorage.
-        Returns the list of City objects linked to the current State.
-        """
-        if models.storage_type != 'db':
-            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+    if models.storage_t != 'db':
+        @property
+        def cities(self):
+            """Getter for cities"""
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
